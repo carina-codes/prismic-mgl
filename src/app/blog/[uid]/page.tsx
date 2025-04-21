ef4b148f83b4a611d66ff37ef5a725eb49cb4e79
carina-codes <carinacodes@gmail.com>
2025-04-20 17:33:43 -0700

    Blog

--- src/app/blog/[uid]/page.tsx
+++ src/app/blog/[uid]/page.tsx
@@ -3,31 +3,21 @@ import { PrismicRichText } from '@prismicio/react';
 
 export default async function BlogPost({ params }: { params: { uid: string } }) {
   const client = createClient();
+  const post = await client.getByUID('blog_post', params.uid);
 
-  try {
-    const post = await client.getByUID('blog_post', params.uid);
+  return (
+    <main className="page-style">
+      <header>
+        <div className="container">
+          <PrismicRichText field={post.data.title} />
+        </div>
+      </header>
 
-    return (
-      <main className="page-style">
-        <header>
-          <div className="container">
-            <PrismicRichText field={post.data.title} />
-          </div>
-        </header>
-
-        <article>
-          <div className="container">
-            <PrismicRichText field={post.data.content} />
-          </div>
-        </article>
-      </main>
-    );
-  } catch (error) {
-    console.error('Error fetching the blog post:', error);
-    return (
-      <div className="container">
-        <h1>Sorry, this post couldn&apos;t be found.</h1>
-      </div>
-    );
-  }
+      <article>
+        <div className="container">
+          <PrismicRichText field={post.data.content} />
+        </div>
+      </article>
+    </main>
+  );
 }
